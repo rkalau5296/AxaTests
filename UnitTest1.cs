d5a5e3772c59f07640c53ea814f8a1c1e08488c9
rkalau5296 <rafal.kalata.itservice@gmail.com>
2020-12-11 22:25:23 +0100

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AxaTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
