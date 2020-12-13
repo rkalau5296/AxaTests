05d55872656e636c3493f4fe9ae029899e73ab60
rkalau5296 <rafal.kalata.itservice@gmail.com>
2020-12-13 16:26:38 +0100

    Add test OnFootFromPlDefiladToChlodna()

--- Tests/GoogleMapsTestSuite.cs
+++ Tests/GoogleMapsTestSuite.cs
@@ -96,6 +96,28 @@ namespace AxaTests
             Assert.IsTrue(distances[1] < 3, "The second route's distances is less then 3 km.");
 
         }
+        [TestMethod]
+        public void OnFootFromPlDefiladToChlodna()
+        {
+            googleMaps.ClickOnFoot();
+            Thread.Sleep(2000);
+            googleMaps.YourLocationInput("plac Defilad 1, 00-901 Warszawa");
+            Thread.Sleep(2000);
+            googleMaps.SearchLoopFrom();
+            Thread.Sleep(2000);
+            googleMaps.TargetLocationInput("Chłodna 51, 00-867 Warszawa");
+            Thread.Sleep(2000);
+            googleMaps.SearchLoopTo();
+            Thread.Sleep(2000);
+            List<int> times = googleMaps.FromChlodnaToPlDefiladTimes();
+            List<double> distances = googleMaps.FromChlodnaToPlDefiladDistances();
+
+            Assert.IsTrue(times[0] < 40, "The first route's time is less then 40 min.");
+            Assert.IsTrue(times[1] < 40, "The second route's time is less then 40 min.");
+            Assert.IsTrue(distances[0] < 3, "The first route's distances is less then 3 km.");
+            Assert.IsTrue(distances[1] < 3, "The second route's distances is less then 3 km.");
+
+        }
         [TestCleanup]
         public void MyTestCleanup()
         {
